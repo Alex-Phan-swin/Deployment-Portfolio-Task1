cb9efe3f0fbaea4484d0c3a24fa4ac0676ca82c6
Alex-Phan-swin <105334704@student.swin.edu.au>
2026-08-12 16:03:10 +1000

    Simple Calculator App

--- CalculatorApp/CalculatorApp/Program.cs
+++ CalculatorApp/CalculatorApp/Program.cs
@@ -1,10 +1,59 @@
-﻿namespace CalculatorApp
+﻿using System.Threading.Channels;
+
+namespace CalculatorApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true) 
+            {
+                Console.Write("Enter first number:");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter Operator (+, -, *, /):");
+                string operation = Console.ReadLine();
+
+                Console.Write("Enter Second number:");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+                double result;
+
+                switch (operation) 
+                {
+                    case  "+":
+                            result = num1 + num2;
+                            break;
+
+                    case  "-":
+                            result = num1 - num2;
+                            break;
+
+                    case  "*":
+                            result = num1 * num2;
+                            break;
+
+                    case  "/":
+                            if (num2 == 0) 
+                            { 
+                                Console.WriteLine("Cannot divideby 0");
+                                continue;
+                            }
+
+                            result = num1 / num2;
+
+                            break;
+
+                    default:
+                        Console.WriteLine("Invalid operator or number");
+                        continue ;
+
+                }
+
+                Console.WriteLine($"Result {result}");
+
+            }
         }
     }
 }
