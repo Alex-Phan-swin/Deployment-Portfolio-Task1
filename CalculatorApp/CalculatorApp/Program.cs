2a37fbebd0819b8d02445257bb91328a4e6fdb2b
Alex-Phan-swin <105334704@student.swin.edu.au>
2026-08-12 18:32:02 +1000

    Mean and json format

--- CalculatorApp/CalculatorApp/Program.cs
+++ CalculatorApp/CalculatorApp/Program.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Channels;
+﻿using MathNet.Numerics;
+using MathNet.Numerics.Statistics;
+using System.Threading.Channels;
+using Newtonsoft.Json;
+using System.Xml;
 
 namespace CalculatorApp
 {
@@ -8,42 +12,56 @@ namespace CalculatorApp
         {
             while (true) 
             {
-                Console.Write("Enter first number:");
+                Console.Write("Enter first number: ");
                 double num1 = Convert.ToDouble(Console.ReadLine());
 
-                Console.Write("Enter Operator (+, -, *, /):");
+                Console.Write("Enter Operator (+, -, *, /, mean):");
                 string operation = Console.ReadLine();
+
+                // Calculate the mean value
+                if (operation == "mean")
+                {
+                    Console.Write("Enter numbers separated by spaces:");
+                    string input = Console.ReadLine();
+
+                    double[] numbers = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+                    double mean_result = numbers.Mean();
+                        
+                    Console.WriteLine($"Mean: {mean_result}");
+                    Console.WriteLine();
+
+                    continue;
+                }
 
                 Console.Write("Enter Second number:");
                 double num2 = Convert.ToDouble(Console.ReadLine());
 
-
                 double result;
 
+                // Standard math operations
                 switch (operation)
                 {
-                    case  "+":
-                            result = num1 + num2;
-                            break;
+                    case "+":
+                        result = num1 + num2;
+                        break;
 
-                    case  "-":
-                            result = num1 - num2;
-                            break;
+                    case "-":
+                        result = num1 - num2;
+                        break;
 
-                    case  "*":
-                            result = num1 * num2;
-                            break;
+                    case "*":
+                        result = num1 * num2;
+                        break;
 
-                    case  "/":
-                            if (num2 == 0) 
-                            { 
-                                Console.WriteLine("Cannot divideby 0");
-                                continue;
-                            }
+                    case "/":
+                        if (num2 == 0) 
+                        {
+                            Console.WriteLine("Cannot divideby 0");
+                            continue;
+                        }
 
-                            result = num1 / num2;
-
-                            break;
+                         result = num1 / num2;
+                         break;
 
                     default:
                         Console.WriteLine("Invalid operator or number");
@@ -51,7 +69,21 @@ namespace CalculatorApp
 
                 }
 
-                Console.WriteLine($"Result {result}");
+                Console.WriteLine($"Result: {result}");
+
+                var calculation = new
+                {
+                    first_num = num1,
+                    second_num = num2,
+                    math_method = operation,
+                    output = result
+                };
+
+                // Use Newtonsoft.Json to convert calculation to JSON
+                string json = JsonConvert.SerializeObject(calculation);
+
+                Console.WriteLine($"JSON: {json}");
+                Console.WriteLine();
 
             }
         }
